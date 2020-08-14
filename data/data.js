ad38d923383d2841db9eed868ad018861a73e0d9
刘天宇 <doubiwan@cpe-172-100-4-16.twcny.res.rr.com>
2020-08-14 16:08:31 +0800

    实例名隐藏

--- data/data.js
+++ data/data.js
@@ -2,7 +2,7 @@ var oracledb = require('oracledb');
 var config = {
   user:'sde',
   password:'sde',
-  connectString : "10.136.6.35:1521/orcl"};oracledb.getConnection(
+  connectString : "xxx:1521/orcl"};oracledb.getConnection(
   config,
   function(err, connection)
   {
--- data/dbAllTime.js
+++ data/dbAllTime.js
@@ -2,7 +2,7 @@ const oracledb = require('oracledb');
 var config = {
     user:'sde',
     password:'sde',
-    connectString : "10.136.6.35:1521/orcl"}
+    connectString : "xxx:1521/orcl"}
     ;
     let allsql = function(valus) {
  
